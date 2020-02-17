b2e3bfa6cfc4dfc79326f9af72860b6faa644818
Jason <jdmarshall@users.noreply.github.com>
2020-02-16 16:27:04 -0800

    Add helmet.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
@@ -15,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
