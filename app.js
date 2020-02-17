8740a63a9d95f62151e5e751bce770fadcbb2f9c
Jason <jdmarshall@users.noreply.github.com>
2020-02-16 16:21:01 -0800

    Switch handlebars templates to .html file format.

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 app.use(logger('dev'));
 app.use(express.json());
